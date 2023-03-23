742fabd54dffed318551e4413e947c13577f98a1
Victor Caio <kaioaraujo426@gmail.com>
2023-03-23 14:37:26 -0300

    FINISHED

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const client = createClient();
 
 const getAllProducts = async () => {
-  const time = Math.random() * 7000;
+  const time = Math.random() * 10000;
 
   return new Promise((resolve) => {
     setTimeout(() => {
@@ -14,8 +14,38 @@ const getAllProducts = async () => {
   });
 };
 
+app.get("/saved", async (request, response) => {
+  await client.del("getAllProducts");
+
+  response.send({ ok: true });
+});
+
 app.get("/", async (request, response) => {
   const productsCache = await client.get("getAllProducts");
+
+  const isProductsCacheStale = !(await client.get("geAllProducts:validation"));
+
+  if (isProductsCacheStale) {
+    const isRefetching = !!(await client.get("getAllProducts:is-refetching"));
+
+    console.log({ isRefetching });
+
+    if (!isRefetching) {
+      await client.set("getAllProducts:is-refetching", "true", { EX: 20 });
+
+      setTimeout(async () => {
+        console.log("Cache is stale - refetching...");
+
+        const products = await getAllProducts();
+
+        await client.set("getAllProducts", JSON.stringify(products));
+
+        await client.set("getAllProducts:validation", "true", { EX: 5 });
+
+        await client.del("getAllProducts:is-refetching");
+      }, 0);
+    }
+  }
 
   if (productsCache) {
     return response.send(JSON.parse(productsCache));
@@ -30,6 +60,10 @@ app.get("/", async (request, response) => {
 
 const startup = async () => {
   await client.connect();
+
+  client.on("error", (error) => {
+    console.log(error);
+  });
 
   app.listen(3434, () => {
     console.log("Server running...");
